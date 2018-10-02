81b0d222f67f8b97cb6b0f9e7b3f51ed341955eb
Krina Soni <krina.soni@openxcellinc.com>
2018-10-02 18:03:30 +0530

    Image Changes

--- src/containers/Page/signin.style.js
+++ src/containers/Page/signin.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { palette } from 'styled-theme';
-import bgImage from '../../image/sign.jpg';
+import bgImage from '../../image/faldax.jpg';
 import WithDirection from '../../settings/withDirection';
 
 const SignInStyleWrapper = styled.div`
@@ -101,9 +101,9 @@ const SignInStyleWrapper = styled.div`
         line-height: 1.2;
         color: ${palette('grayscale', 1)};
         padding-left: ${props =>
-          props['data-rtl'] === 'rtl' ? 'inherit' : '13px'};
+    props['data-rtl'] === 'rtl' ? 'inherit' : '13px'};
         padding-right: ${props =>
-          props['data-rtl'] === 'rtl' ? '13px' : 'inherit'};
+    props['data-rtl'] === 'rtl' ? '13px' : 'inherit'};
         margin: 15px 0;
         position: relative;
         display: flex;
