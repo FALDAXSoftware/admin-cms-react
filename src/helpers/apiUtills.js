e4b88b7163e1dbbc42af669717a009648e8efc4a
Nevil Macwan <nevil.macwan@openxcellinc.com>
2019-10-21 10:31:41 +0000

    Update apiUtills.js

--- src/helpers/apiUtills.js
+++ src/helpers/apiUtills.js
@@ -3,7 +3,8 @@ import { stat } from "fs";
 //const API_URL = "http://192.168.3.32:1337"; // Local (Krina) URL
 //const API_URL = "http://192.168.2.224:1337"; // Local (Kalpit) URL
 //const API_URL = "http://192.168.1.96:1337"; //Local Jagdish URL
-const API_URL = "https://dev-backend.faldax.com"; //Live Client URL
+const API_URL = "https://pre-prod-backend.faldax.com"; //Pre-production URL
+//const API_URL = "https://dev-backend.faldax.com"; //Live Client URL
 //const API_URL = "https://prod-backend.faldax.com"; //Live Client URL
 
 const ApiUtils = {
