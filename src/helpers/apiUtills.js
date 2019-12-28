5b227eff873c0446827d5c9f1a5c4a921ec604d8
Jagdish Banda <jagdish.banda@openxcelltechnolabs.com>
2019-12-28 09:56:55 +0000

    Update to mainnet url by Jagdish

--- src/helpers/apiUtills.js
+++ src/helpers/apiUtills.js
@@ -1,10 +1,4 @@
-// const API_URL = "http://192.168.0.213:1337"; // Local (Mansi) URL
-//const API_URL = "http://192.168.3.32:1337"; // Local (Krina) URL
-// const API_URL = "http://192.168.0.224:1337"; // Local (Kalpit) URL
-// const API_URL = "http://192.168.1.96:1337"; //Local Jagdish URL
-// const API_URL = "https://dev-backend.faldax.com"; //Live Client URL
-const API_URL = "https://pre-prod-backend.faldax.com"; //Preprod URL
-// const API_URL = "https://prod-backend.faldax.com"; //Live Client URL
+const API_URL = "https://mainnet-backend.faldax.com"; //Mainnet URL
 
 const ApiUtils = {
   //super admin sign in api
