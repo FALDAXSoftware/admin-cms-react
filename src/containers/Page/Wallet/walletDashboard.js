f04cc3473e8280312522f3685f545ad6f0cf1f8c
OpenXcell <openxcell@OpenXcells-Mac-mini.local>
2020-02-28 15:28:49 +0530

    personal details and wallet dashboard changesa

--- src/containers/Page/Wallet/walletDashboard.js
+++ src/containers/Page/Wallet/walletDashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import LayoutWrapper from '../../../components/utility/layoutWrapper';
 import { Tabs } from 'antd';
 import { TabPane } from '../../../components/uielements/tabs';
-import {  withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import WalletFaldaxDashboard from './faldax/walletFaldaxDashboard';
 // import WalletWarmDashboard from './warm/walletWarmDashboard';
 // import WalletCustodialDashboard from './custodial/walletCustodialDashboard';
@@ -12,22 +12,22 @@ import { isAllowed } from '../../../helpers/accessControl';
 import WalletHotReceiveDashboard from "./hot-receive/walletDashboard"
 import { BreadcrumbComponent } from '../../Shared/breadcrumb';
 class WalletDashboard extends Component {
-    state = {  }    
-    render() { 
+    state = {}
+    render() {
         return (
-        <LayoutWrapper>
-            {/* <BackButton {...this.props}/> */}
-            <BreadcrumbComponent {...this.props}/>
-            <Tabs className="full-width">
-                {isAllowed("admin_wallet_fees_details") &&<TabPane tab="FALDAX Wallet" key="1"><WalletFaldaxDashboard/></TabPane>}
-                {/* {isAllowed("admin_warm_wallet_data") &&<TabPane tab="Warm Wallet" key="2"><WalletWarmDashboard/></TabPane>}
+            <LayoutWrapper>
+                {/* <BackButton {...this.props}/> */}
+                <BreadcrumbComponent {...this.props} />
+                <Tabs className="full-width">
+                    {isAllowed("admin_wallet_fees_details") && <TabPane tab="FALDAX Wallet" key="1"><WalletFaldaxDashboard /></TabPane>}
+                    {/* {isAllowed("admin_warm_wallet_data") &&<TabPane tab="Warm Wallet" key="2"><WalletWarmDashboard/></TabPane>}
                 {isAllowed("admin_cold_wallet_data") &&<TabPane tab="Custodial Wallet" key="3"><WalletCustodialDashboard/></TabPane>}
                 {isAllowed("admin_hotsend_wallet_data") &&<TabPane tab="Hot Send Wallet" key="4"><WalletHotSendDashboard/></TabPane>} */}
-                {isAllowed("admin_hotreceive_wallet_data") &&<TabPane tab="Hot Receive Wallet" key="5"><WalletHotReceiveDashboard/></TabPane>}
-            </Tabs>
-        </LayoutWrapper>
+                    {isAllowed("admin_hotreceive_wallet_data") && <TabPane tab="Main Wallet" key="5"><WalletHotReceiveDashboard /></TabPane>}
+                </Tabs>
+            </LayoutWrapper>
         );
     }
 }
- 
+
 export default withRouter(WalletDashboard);
